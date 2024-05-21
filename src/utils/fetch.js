5e6d22b6691eb096b354616ee4fc067cc36e9def
Bryan <bryanfinesw@gmail.com>
2024-05-21 10:03:46 -0400

    feat: added fetch functions

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,27 @@
-export default async function fetchData() {
+import "dotenv/config";
+
+export async function fetchCurrentData(search = "London") {
   try {
-    return "peanut butter";
+    const key = process.env.API_KEY;
+    const response = await fetch(
+      `${process.env.BASE_URL}current.json?key=${key}&q=${search}&aqi=no`
+    );
+    const data = response.json();
+    return data;
   } catch (err) {
-    throw Error(err);
+    throw new Error(err);
+  }
+}
+
+export async function fetchForecastData(search = "London", days = null) {
+  try {
+    const key = process.env.API_KEY;
+    const response = await fetch(
+      `${process.env.BASE_URL}forecast.json?key=${key}&q=${search}&days=${days}&aqi=no&alerts=no`
+    );
+    const data = response.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
   }
 }
--- tests/fetch.test.js
+++ tests/fetch.test.js
@@ -1,14 +1,24 @@
-import fetchData from "../src/utils/fetch";
+import { fetchCurrentData, fetchForecastData } from "../src/utils/fetch";
 
-test("the data is peanut butter", async () => {
-  const data = await fetchData();
-  expect(data).toBe("peanut butter");
+test("Does fetch exist", () => {
+  expect(fetchCurrentData);
 });
 
-test("the fetch fails with an error", async () => {
-  try {
-    await fetchData();
-  } catch (error) {
-    expect(error).toMatch("error");
-  }
+test("Does fetch retrieve data", async () => {
+  const data = await fetchCurrentData("current.json");
+  expect(data).toBe(data);
+});
+
+test("Does search paramater work", async () => {
+  const data = await fetchCurrentData("Virginia");
+  expect(data.location.name).toMatch("Virginia Beach");
+});
+
+test("Does fetch forecast exist", () => {
+  expect(fetchForecastData);
+});
+
+test("Does fetch forecast retrieve forecast data", async () => {
+  const data = await fetchForecastData("Virginia", 3);
+  expect(data.forecast.forecastday[0].date).toMatch("2024-05-21");
 });
