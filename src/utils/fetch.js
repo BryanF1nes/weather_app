0f31916f91c724b7e3dc9e64dc8c3fa32690eee6
Bryan <bryanfinesw@gmail.com>
2024-05-22 11:47:11 -0400

    fixed: fetch

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,7 +2,7 @@ export async function fetchCurrentData(search = "London") {
   try {
     const key = process.env.API_KEY;
     const response = await fetch(
-      `${process.env.BASE_URL}current.json?key=${key}&q=${search}&aqi=no`
+      `https://api.weatherapi.com/v1/current.json?key=${key}&q=${search}&aqi=no`
     );
     const data = await response.json();
     return data;
@@ -15,7 +15,7 @@ export async function fetchForecastData(search = "London") {
   try {
     const key = process.env.API_KEY;
     const response = await fetch(
-      `${process.env.BASE_URL}forecast.json?key=${key}&q=${search}&days=3&aqi=no&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${search}&days=3&aqi=no&alerts=no`
     );
     const data = await response.json();
     return data.forecast.forecastday;
