89fa6c1dfa9726724f2ecc6eea56726c91c013a8
Bryan <bryanfinesw@gmail.com>
2024-05-22 11:46:21 -0400

    Initial dist subtree commit

--- src/modules/createWebpage.js
+++
@@ -1,7 +0,0 @@
-import { root } from "../index.js";
-import { createForm } from "./form.js";
-
-export const createWebpage = () => {
-  root.classList.add("day-time");
-  root.appendChild(createForm());
-};
